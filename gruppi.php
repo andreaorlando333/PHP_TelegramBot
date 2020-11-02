d91fa3cd97fd7956a76b097a243ac6a1475f7b73
Andrea Orlando <71812411+andreaorlando333@users.noreply.github.com>
2020-11-02 08:17:19 +0100

    Update gruppi.php

--- gruppi.php
+++ gruppi.php
@@ -1,10 +1,6 @@
 <?php
 
-/*
-PLUGIN GRUPPI
-Versione 3.0
-*/
-echo "<br>Plugin Gruppi: 3.0";
+
 
 $replyID = $update["message"]["reply_to_message"]["from"]["id"];
 $replyNome = $update["message"]["reply_to_message"]["from"]["first_name"];
